a273063a5539d3c957e6cd1856f74c91f81d251d
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-20 09:00:24 -0300

    started refactoring with redux; redux configured.

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ function App() {
     <div className="App">
       <h1>Exercício React-Redux (Simples)</h1>
       <div className='linha'>
-        <Intervalo min={min} max={max} onMinChanged={setMin} onMaxChanged={setMax}></Intervalo>
+        <Intervalo onMinChanged={setMin} onMaxChanged={setMax}></Intervalo>
       </div>
       <div className='linha'>
-        <Media min={min} max={max}></Media>
-        <Soma min={min} max={max}></Soma>
-        <Sorteio min={min} max={max}></Sorteio>
+        <Media></Media>
+        <Soma></Soma>
+        <Sorteio></Sorteio>
       </div>
     </div>
   );
